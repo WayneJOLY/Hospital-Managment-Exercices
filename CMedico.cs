6dac97b8e20b880b51d04c9016ec6fb39244c66c
Gerald <geraldjoly191@gmail.com>
2024-07-16 12:16:29 -0300

    Inplementé la clase CMedico que hereda de CEmpleado

---
+++ CMedico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Interzonal_de_Haedo
+{
+    public class CMedico : CPersonalDeSanidad
+    {
+        private uint legajo;
+        private string nombre;
+        private string apellido;
+        private uint aniDeIngreso;
+        private string categoriaProfesional;
+        private uint numeroDeMatricula;
+        private string especialidad;
+        private string servicio;  //PARA SABER EN QUÉ SERVICIO ESTA TRABAJANDO
+
+        public CMedico(uint legajo, string nombre, string apellido, uint aniDeIngreso, string categoriaProfesional, uint numeroDeMatricula,string especialidad,string servicio) : base(legajo, nombre, apellido, aniDeIngreso,categoriaProfesional,numeroDeMatricula)
+        { 
+            this.especialidad = especialidad ;
+            this.servicio = servicio ;
+        }
+
+
+
+        public override string ToString()
+        {
+            string datos = base.ToString();
+            datos += "Especialidad :" + this.especialidad + "Servicio :" + this.servicio;
+            return datos;
+        }
+    }
+}
