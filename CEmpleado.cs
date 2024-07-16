cf3d0e5e4b117c27c45b28e89b76c9bef5059f3b
Gerald <geraldjoly191@gmail.com>
2024-07-16 12:15:09 -0300

    Emplementé la clase CEmpleado

---
+++ CEmpleado.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Interzonal_de_Haedo
+{
+    public class CEmpleado
+    {
+        private uint legajo;
+        private string nombre;
+        private string apellido;
+        private uint aniDeIngreso;
+
+        public CEmpleado(uint legajo, string nombre, string apellido, uint aniDeIngreso)
+        {
+            this.legajo = legajo;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.aniDeIngreso = aniDeIngreso;
+        }
+
+        public CEmpleado(uint legajo) 
+        {
+            this.legajo=legajo;
+        }
+
+        public uint GetLegajo()
+        {
+            return this.legajo;
+        }
+
+        public string GetNombre() { return this.nombre; }
+        public string GetApellido() { return this.apellido; }
+        public uint GetAnioDeIngreso() {  return this.aniDeIngreso; }
+
+        public void SetLegajo(uint legajo) { this.legajo= legajo; }
+        public void SetNombre(string nombre) {  this.nombre = nombre; }
+        public void SetApellido(string Apellido) { this.apellido = Apellido; }
+
+
+        public override string ToString()
+        {
+            return "\nNombre :" + this.nombre + "Apellido :"+ this.apellido + "Legajo :" + this.legajo.ToString() + "Año de Ingreso :"+this.aniDeIngreso.ToString();
+        }
+    }
+}
