018a350d8fb99758564e7116c9ecdc2e69d0961b
Gerald <geraldjoly191@gmail.com>
2024-07-25 02:11:33 -0300

    agrequé Funciones y Modifiqué algunas

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace Hospital_Interzonal_de_Haedo
             string categoriaProfesional, especialidad;
             uint numeroDeMatricula;
 
+            Random rand = new Random();
+
             Console.WriteLine("Hello, World!");
 
             char opcion,eleccion;
@@ -31,10 +33,10 @@ namespace Hospital_Interzonal_de_Haedo
                     case '2':
                             Console.WriteLine("Agregar Un Empleado");
 
-                            legajo=interfaz.DevolverUnUint("Ingrese el Legajo de Empleado");
-                            nombre=interfaz.DevlverUnString("Ingrese el Nombre de Empleado");
-                            apellido = interfaz.DevlverUnString("Ingrese el Apellido de Empleado");
-                            aniDeIngreso =interfaz.DevolverUnUint("Ingrese el Año  de ingreso del Empleado");
+                        legajo = (uint)rand.Next(1500, 60796);       //interfaz.DevolverUnUint("Ingrese el Legajo de Empleado");
+                        nombre = GenerarNombreAleatorio();     //interfaz.DevlverUnString("Ingrese el Nombre de Empleado");
+                        apellido = GenerarApellidoAleatorio();   //interfaz.DevlverUnString("Ingrese el Apellido de Empleado");
+                        aniDeIngreso = (uint)rand.Next(1970, 2024); //interfaz.DevolverUnUint("Ingrese el Año  de ingreso del Empleado");
 
                             Console.WriteLine("\n QUÉ TIPO DE EMPLEADO QUIERE REGISTRAR ? \n [A] Personal de Sanidad \n [B] Personal de Apoyo ");
                             eleccion=char.Parse(Console.ReadLine());
@@ -42,11 +44,13 @@ namespace Hospital_Interzonal_de_Haedo
                         switch (eleccion)
                         {
                             case'A':
-                                categoriaProfesional = interfaz.DevlverUnString("Ingrese la Categoria Profecional");
-                                numeroDeMatricula = interfaz.DevolverUnUint("Ingrese el Numero de Matricula");
+                                categoriaProfesional = GenerarCategoriaProfecionalAleatorio(); //interfaz.DevlverUnString("Ingrese la Categoria Profecional");
+                                numeroDeMatricula = (uint)rand.Next(190000, 900000); //interfaz.DevolverUnUint("Ingrese el Numero de Matricula");
+                                CPersonalDeSanidad sanidad= new CPersonalDeSanidad(legajo,nombre,apellido,aniDeIngreso,categoriaProfesional,numeroDeMatricula);
+                                hospital.AgregarEmpleado(sanidad);
                                 break;
                             case 'B':
-                                puesto = interfaz.DevlverUnString("Ingrese el puesto de Empleado");
+                                puesto = GenerarPuesto(); //interfaz.DevlverUnString("Ingrese el puesto de Empleado");
                                 CPersonalDeApoyo Papoyo= new CPersonalDeApoyo(legajo,nombre,apellido,aniDeIngreso,puesto);
                                 hospital.AgregarEmpleado(Papoyo);
                                 break;
@@ -60,39 +64,165 @@ namespace Hospital_Interzonal_de_Haedo
                                 Console.ReadLine();
                         break;
                     case '4':
-                        codigo = interfaz.DevlverUnString("Ingrese el Codigo del Servicio");
-                        NombreDelServicio = interfaz.DevlverUnString("Ingrese el Nombre del Servicio");
-                        legajo = interfaz.DevolverUnUint("Ingrese el Legajo de JEFE de Servicio");
-                        CEmpleado jefe=hospital.BuscarEmpleado(legajo);
-                        CServicio SER;
-                        if (jefe==null)
-                        {
-                            legajo = interfaz.DevolverUnUint("Ingrese el Legajo del Jefe de Servicio");
-                            nombre = interfaz.DevlverUnString("Ingrese el Nombre del Jefe de Servicio");
-                            apellido = interfaz.DevlverUnString("Ingrese el Apellido del Jefe de Servicio");
-                            aniDeIngreso = interfaz.DevolverUnUint("Ingrese el Año  de ingreso del Jefe de Servicio");
-                            categoriaProfesional = interfaz.DevlverUnString("Ingrese la Categoria Profecional del Jefe de Servicio");
-                            numeroDeMatricula = interfaz.DevolverUnUint("Ingrese el Numero de Matricula del Jefe de Servicio");
-                            especialidad = interfaz.DevlverUnString("Ingrese la Especialidad del Jefe de Servicio");
-                            jefe=new CMedico(legajo,nombre,apellido,aniDeIngreso,categoriaProfesional,numeroDeMatricula,especialidad,NombreDelServicio);
-                            SER = new CServicio(codigo, nombre, jefe);
-                            hospital.AgregarServicio(SER);
-                        }
+                                    codigo = (rand.Next(1000, 90000)).ToString(); //interfaz.DevlverUnString("Ingrese el Codigo del Servicio");
+                                    NombreDelServicio = GenerarNombreDeSercvicioAleatorio(); //interfaz.DevlverUnString("Ingrese el Nombre del Servicio");
+                                    legajo = (uint)interfaz.DevolverUnUint("Ingrese el Legajo de JEFE de Servicio");
+                                    CEmpleado jefe=hospital.BuscarEmpleado(legajo);
+                                    CServicio SER;
+                                    if (jefe==null)
+                                    {
+                                        legajo = (uint)rand.Next(1500, 60796);//interfaz.DevolverUnUint("Ingrese el Legajo del Jefe de Servicio");
+                                        nombre = GenerarNombreAleatorio();//interfaz.DevlverUnString("Ingrese el Nombre del Jefe de Servicio");
+                                        apellido = GenerarApellidoAleatorio(); //interfaz.DevlverUnString("Ingrese el Apellido del Jefe de Servicio");
+                                        aniDeIngreso = (uint)rand.Next(1970, 2024); //interfaz.DevolverUnUint("Ingrese el Año  de ingreso del Jefe de Servicio");
+                                        categoriaProfesional = GenerarCategoriaProfecional(); //interfaz.DevlverUnString("Ingrese la Categoria Profecional del Jefe de Servicio");
+                                        numeroDeMatricula = (uint)rand.Next(10500, 60796); //interfaz.DevolverUnUint("Ingrese el Numero de Matricula del Jefe de Servicio");
+                                        especialidad = GenerarEspecialidadMedicaProfecionalAleatorio(); //interfaz.DevlverUnString("Ingrese la Especialidad del Jefe de Servicio");
+                                        jefe =new CMedico(legajo,nombre,apellido,aniDeIngreso,categoriaProfesional,numeroDeMatricula,especialidad,NombreDelServicio);
+                                        SER = new CServicio(codigo, NombreDelServicio, (CMedico)jefe);
+                                        hospital.AgregarServicio(SER);
+                                        hospital.AgregarEmpleado(jefe);
+                                    }
 
-                        if(jefe is CMedico)
-                        {
-                            SER = new CServicio(codigo, NombreDelServicio, jefe);
-                            hospital.AgregarServicio(SER);
-                        }
-                        
+                                    //if(jefe is CMedico)
+                                    //{
+                                    //    SER = new CServicio(codigo, NombreDelServicio, (CMedico)jefe);
+                                    //    hospital.AgregarServicio(SER);
+                                    //}
+                                    //else
+                                    //{
+                                    //    Console.WriteLine("El  JEFE DEBE OBLIGATORIAMENTE SER UN MEDICO");
+                                    //}
                         break;
                     case '5':
+                        legajo = (uint)interfaz.DevolverUnUint("Ingrese el Legajo de Empleado");
+                        codigo = interfaz.DevlverUnString("Ingrese el Codigo Del Servicio");
+                        CEmpleado empleado=hospital.BuscarEmpleado(legajo);
+                        CServicio servicio = hospital.BuscarServicio(codigo);
+                        hospital.AgregarEmpleadoAServicio(empleado, servicio);
                         break;
                     case '6':
+                        codigo = interfaz.DevlverUnString("Ingrese el Codigo Del Servicio");
+                        legajo = (uint)interfaz.DevolverUnUint("Ingrese el Legajo de Empleado");
+                        empleado = hospital.BuscarEmpleado(legajo);
+                        servicio = hospital.BuscarServicio(codigo);
+                        if (empleado is CMedico)
+                        {
+                            servicio.setJefe((CMedico) empleado);
+                        }
                         break;
+
+                    case '7':
+                        Console.WriteLine(hospital.ListaDeServiciosDelHospital());
+                        Console.ReadKey();
+                        break;
+
                 }
 
             }while (opcion != '0');
+        }
+
+
+
+
+
+
+        static string GenerarNombreAleatorio()
+        {
+            string[] nombres = { "Juan", "María", "Pedro", "Ana", "Luis", "Laura", "Carlos", "Marta", "Javier", "Lucía" };
+            Random rnd = new Random();
+            return nombres[rnd.Next(nombres.Length)];
+        }
+
+        // Función para generar un apellido aleatorio
+        static string GenerarApellidoAleatorio()
+        {
+            string[] apellidos = { "García", "Martínez", "López", "González", "Rodríguez", "Fernández", "Pérez", "Gómez", "Sánchez", "Díaz" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+
+        static string GenerarCategoriaProfecional()
+        {
+            string[] apellidos = { "García", "Martínez", "López", "González", "Rodríguez", "Fernández", "Pérez", "Gómez", "Sánchez", "Díaz" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+
+        static string GenerarPuesto()
+        {
+            string[] apellidos = { "Cordinador", "Gestion de Talento", "Gestión digital de documentación", "personal de limpieza", "personal demantenimiento", "Farmacéuticos", "Facturistas", "Contabilidad", "Informática y Sistemas", "Comunicación y Marketing" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+
+
+        static string GenerarCategoriaProfecionalAleatorio()
+        {
+            string[] apellidos = { "Medico", "Enfermero", "Tecnico" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+
+        static string GenerarEspecialidadMedicaProfecionalAleatorio()
+        {
+            string[] apellidos = { "Guardia Medica", "Traumatologia", "Cirugia","Pediatria","Oncologia" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+        static string GenerarEspecializacionEnfermeriaAleatorio()
+        {
+            string[] apellidos = { "Enfermeria General", "Terapista Intensivo", "Cirugia", "Pediatria", "Oncologia","Dermatologia", "Psiquiatría" };
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
+        }
+        static string GenerarNombreDeSercvicioAleatorio()
+        {
+            string[] apellidos = { "Emergencias", "Hospitalización", "Consulta Externa", "Quirófanos", "Unidad de Cuidados Intensivos", "Laboratorio Clínico", "Imágenes Médicas" };
+            /*
+             * 1. Emergencias
+Atención inmediata para casos críticos y urgentes.
+2. Consulta Externa
+Consultas programadas con médicos especialistas.
+3. Hospitalización
+Servicios de internación para pacientes que requieren atención continua.
+Servicios Quirúrgicos
+4. Quirófanos
+Realización de cirugías programadas y de emergencia.
+5. Unidad de Cuidados Intensivos (UCI)
+Atención especializada para pacientes en estado crítico.
+Servicios Diagnósticos
+6. Laboratorio Clínico
+Análisis de muestras biológicas para diagnósticos.
+7. Imágenes Médicas
+Rayos X, ultrasonido, tomografía computarizada (TC), resonancia magnética (RM).
+Servicios de Rehabilitación
+8. Fisioterapia
+Tratamientos para la recuperación física de los pacientes.
+9. Terapia Ocupacional
+Ayuda a los pacientes a recuperar habilidades para la vida diaria.
+Servicios de Apoyo
+10. Farmacia
+Suministro de medicamentos y asesoramiento farmacéutico.
+11. Nutrición y Dietética
+Planificación de dietas adecuadas para pacientes.
+12. Psicología y Salud Mental
+Apoyo psicológico y psiquiátrico para pacientes.
+Servicios Administrativos
+13. Admisión y Registro
+Proceso de ingreso de pacientes y gestión de datos.
+14. Facturación
+Gestión de costos y cobros por servicios médicos.
+Servicios Especializados
+15. Oncología
+Tratamiento y manejo de pacientes con cáncer.
+16. Pediatría
+Atención médica especializada para niños.
+17. Ginecología y Obstetricia
+Atención de la salud reproductiva de mujeres y cuidado prenatal.
+             * */
+            Random rnd = new Random();
+            return apellidos[rnd.Next(apellidos.Length)];
         }
     }
 }
