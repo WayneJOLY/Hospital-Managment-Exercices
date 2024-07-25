5216f23b4d188ecec5240d9c67789c550bc2bcb1
Gerald <geraldjoly191@gmail.com>
2024-07-25 02:09:32 -0300

    Corrigé Errores de tipeo

--- CServicio.cs
+++ CServicio.cs
@@ -9,24 +9,25 @@ namespace Hospital_Interzonal_de_Haedo
 {
     public class CServicio
     {
-        private string codiogo;
-        private string nombre;
-        private CEmpleado jefe;
+        private string codigo;
+        private string nombreDeServicio;
+        private CMedico jefe;
         private ArrayList ListaDeEmpleadosDelServicio;
-        public CServicio(string codiogo, string nombre, CEmpleado jefe)
+        public CServicio(string codigo, string nombre, CMedico jefe)
         {
-            this.codiogo = codiogo;
-            this.nombre = nombre;
+            this.codigo = codigo;
+            this.nombreDeServicio = nombre;
             this.jefe = jefe;
 
             ListaDeEmpleadosDelServicio = new ArrayList();
         }
 
-        public string getCodigo() { return this.codiogo; }
-        public string getNombre() { return this.nombre; }
-        public CEmpleado getCempleado() { return this.jefe; }
-
-        public CEmpleado BuscarEmpleado( uint legajo )
+        public string getCodigo() { return this.codigo; }
+        public string getNombre() { return this.nombreDeServicio; }
+        public CMedico getJefe() { return this.jefe; }
+        
+        public void setJefe(CMedico jefe) {  this.jefe = jefe; }
+        public CEmpleado? BuscarEmpleado( uint legajo )
         {
             foreach(CEmpleado empleado in ListaDeEmpleadosDelServicio)
             {
@@ -41,7 +42,7 @@ namespace Hospital_Interzonal_de_Haedo
 
         public bool AgreagEmpeado(CEmpleado empleado)
         {
-            CEmpleado aux =BuscarEmpleado(empleado.GetLegajo());
+            CEmpleado aux = BuscarEmpleado(empleado.GetLegajo());
 
             if(aux == null)
             {
@@ -68,7 +69,30 @@ namespace Hospital_Interzonal_de_Haedo
 
             foreach(CEmpleado empleado in ListaDeEmpleadosDelServicio)
             {
-                datos += empleado.ToString();
+            
+                if (empleado is CMedico)
+                {
+                    CMedico medico = (CMedico)empleado;
+                    datos += medico.ToString();
+                }
+                else
+                if (empleado is CPersonalDeApoyo)
+                {
+                    CPersonalDeApoyo medico = (CPersonalDeApoyo)empleado;
+                    datos += medico.ToString();
+                }
+                else
+                if (empleado is CPersonalDeSanidad)
+                {
+                    CPersonalDeSanidad medico = (CPersonalDeSanidad)empleado;
+                    datos += medico.ToString();
+                }
+                else if (empleado is CEmpleado)
+                {
+
+                    datos += empleado.ToString();
+                }
+                //AUN NO TERMINA
             }
 
             return datos;
@@ -76,7 +100,9 @@ namespace Hospital_Interzonal_de_Haedo
         public override string ToString()
         {
             string datos = "";
-            datos += "\n Nombre :" + this.nombre + "\nCodigo :" + this.codiogo + "\nJefe :" + this.jefe.ToString();
+            datos += "\n Nombre :" + this.nombreDeServicio + "\nCodigo :" + this.codigo + "\nJefe :" + this.jefe.ToString();
+            datos += "\n LISTA DE TRABAJADORES \n";
+            datos += ListaDeLosEmpleados();
             return datos ;
         }
     }
