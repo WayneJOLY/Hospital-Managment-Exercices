d02b47c7905619110b19cf8a9ff2fa7167e0fba9
Gerald <geraldjoly191@gmail.com>
2024-07-29 10:54:58 -0300

    Agrequé el Atributo Monto y el Metodo Haber Mensual() para calcular los sueldos de los empleados

--- CEmpleado.cs
+++ CEmpleado.cs
@@ -13,6 +13,9 @@ namespace Hospital_Interzonal_de_Haedo
         private string apellido;
         private uint aniDeIngreso;
         public bool ESTA_EN_UN_SERVIVCIO;
+        private static float monto;//
+
+
         public CEmpleado(uint legajo, string nombre, string apellido, uint aniDeIngreso)
         {
             this.legajo = legajo;
@@ -39,10 +42,35 @@ namespace Hospital_Interzonal_de_Haedo
         public void SetNombre(string nombre) {  this.nombre = nombre; }
         public void SetApellido(string Apellido) { this.apellido = Apellido; }
 
+        public static float getMonto()
+        {
+            return monto;
+        }
+        public static void setMonto(float monto)
+        {
+            CEmpleado.monto = monto;
+        }
+        /*---------------- INICIO HABER MENSUAL ---------------------*/
+        public float HaberMensual()
+        {
+            float total = 0;
+            uint antiguedad = 2024 - aniDeIngreso;
+            if (antiguedad < 2)                      { total = monto * (float)0.5; }
+            if (antiguedad >= 2 && antiguedad < 4)   { total =  monto * (float)1.10; }
+            if (antiguedad >= 8 && antiguedad < 10)  { total =  monto * (float)1.3; }
+            if (antiguedad >= 10 && antiguedad < 12) { total = monto * (float)1.5; }
+            if (antiguedad >= 12 && antiguedad < 14) { total = monto * (float)1.70; }
+            if (antiguedad >= 14 && antiguedad < 16) { total = monto * (float)1.9; }
+            if (antiguedad >= 16 && antiguedad < 18) { total = monto * (float)2.1; }
+            if (antiguedad >= 18 && antiguedad < 20) { total = monto * (float)2.3; }
+            if (antiguedad >= 20)                    { total = monto * (float)2.5; }
 
+            return total;
+        }
+        /*------- FIN HABER MENSUAL  -----*/
         public override string ToString()
         {
-            return "\n " + this.nombre + " "+ this.apellido + " " + this.legajo.ToString() + " "+this.aniDeIngreso.ToString();
+            return "\n " + this.nombre + " "+ this.apellido + " " + this.legajo.ToString() + " "+this.aniDeIngreso.ToString() + "\t Haber "+this.HaberMensual().ToString();
         }
     }
 }
